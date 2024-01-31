3a41c36a80e29b7fb6f5509a890e7991cf1281a1
shrenikm <shrenik95@gmail.com>
2024-01-30 23:11:23 -0800

    init config

--- python/common/manipulator_pliants/lite6_pliant.py
+++ python/common/manipulator_pliants/lite6_pliant.py
@@ -1,7 +1,22 @@
-
-
+import attr
+from pydrake.geometry import SceneGraph
+from pydrake.multibody.plant import (
+    AddMultibodyPlantSceneGraph,
+    MultibodyPlant,
+)
 from pydrake.systems.framework import Diagram
 
 
-def create_lite6_pliant() -> Diagram:
-    ...
+@attr.frozen
+class Lite6PliantConfig:
+    run_on_hardware: bool
+    time_step_s: float
+
+
+def create_lite6_pliant(config: Lite6PliantConfig) -> Diagram:
+
+    builder = DiagramBuilder()
+    plant: MultibodyPlant, scene_graph: SceneGraph = AddMultibodyPlantSceneGraph(
+        builder,
+        time_step=config.time_step_s,
+    )

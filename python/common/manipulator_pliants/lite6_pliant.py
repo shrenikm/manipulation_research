96da4c719f58e8d328647ae384828b6a5c74cd0b
shrenikm <shrenik95@gmail.com>
2024-01-31 23:53:34 -0800

    config

--- python/common/manipulator_pliants/lite6_pliant.py
+++ python/common/manipulator_pliants/lite6_pliant.py
@@ -1,22 +1,37 @@
+from typing import Optional
+
 import attr
 from pydrake.geometry import SceneGraph
 from pydrake.multibody.plant import (
     AddMultibodyPlantSceneGraph,
+    ApplyMultibodyPlantConfig,
     MultibodyPlant,
+    MultibodyPlantConfig,
 )
-from pydrake.systems.framework import Diagram
+from pydrake.systems.framework import Diagram, DiagramBuilder
+
+from python.lite6.utils.lite6_model_utils import Lite6ModelType
 
 
 @attr.frozen
 class Lite6PliantConfig:
+    model_type: Lite6ModelType
     run_on_hardware: bool
     time_step_s: float
+    plant_config: Optional[MultibodyPlantConfig] = None
 
 
 def create_lite6_pliant(config: Lite6PliantConfig) -> Diagram:
 
-    builder = DiagramBuilder()
-    plant: MultibodyPlant, scene_graph: SceneGraph = AddMultibodyPlantSceneGraph(
+    builder: DiagramBuilder = DiagramBuilder()
+    plant: MultibodyPlant
+    scene_graph: SceneGraph
+    plant, scene_graph = AddMultibodyPlantSceneGraph(
         builder,
         time_step=config.time_step_s,
     )
+
+    if config.plant_config is not None:
+        ApplyMultibodyPlantConfig(config.plant_config, plant)
+
+    # Load the model

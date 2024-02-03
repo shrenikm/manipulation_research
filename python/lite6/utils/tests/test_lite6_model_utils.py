19fd311e0b7d531547f53f8fb685b0af92da8e64
shrenikm <shrenik95@gmail.com>
2024-02-02 23:01:54 -0800

    Test

--- python/lite6/utils/tests/test_lite6_model_utils.py
+++ python/lite6/utils/tests/test_lite6_model_utils.py
@@ -5,10 +5,11 @@ import pytest
 from python.lite6.utils.lite6_model_utils import (
     Lite6ModelType,
     get_drake_lite6_urdf_path,
+    get_lite6_urdf_base_frame_name,
 )
 
 
-def test_lite6_model_paths() -> None:
+def test_get_drake_lite6_urdf_path() -> None:
     """
     Test that each a valid and existing model path can be obtained
     for each lite6 model type.
@@ -18,6 +19,14 @@ def test_lite6_model_paths() -> None:
             lite6_model_type=lite6_model_type,
         )
         assert os.path.isfile(urdf_path)
+
+
+def test_lite6_urdf_base_frame_name() -> None:
+    for lite6_model_type in Lite6ModelType:
+        base_frame_name = get_lite6_urdf_base_frame_name(
+            lite6_model_type=lite6_model_type,
+        )
+        assert isinstance(base_frame_name, str)
 
 
 if __name__ == "__main__":

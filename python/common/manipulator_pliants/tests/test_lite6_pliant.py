919eeba909d0cd3294c16078e4b93bd703d67323
shrenikm <shrenik95@gmail.com>
2024-02-15 22:58:25 -0800

    ok

--- python/common/manipulator_pliants/tests/test_lite6_pliant.py
+++ python/common/manipulator_pliants/tests/test_lite6_pliant.py
@@ -1,14 +1,33 @@
 import pytest
 
+from python.common.control.constructs import PIDGains
+from python.common.manipulator_pliants.lite6_pliant import (
+    Lite6PliantConfig,
+    create_lite6_pliant,
+)
 from python.common.testing_utils import execute_pytest_file
+from python.lite6.utils.lite6_model_utils import Lite6ModelType
 
 
-def test_create_lite6_pliant() -> None:
-    ...
+@pytest.mark.parametrize(
+    "unsupported_model_type",
+    [Lite6ModelType.ROBOT_WITH_V_GRIPPER, Lite6ModelType.ROBOT_WITHOUT_GRIPPER],
+)
+def test_create_lite6_pliant_with_unsupported_model_type(
+    unsupported_model_type: Lite6ModelType,
+) -> None:
 
+    id_pid_gains = PIDGains(kp=1.0, ki=1.0, kd=1.0)
 
-def test_a() -> None:
-    ...
+    config = Lite6PliantConfig(
+        lite6_model_type=unsupported_model_type,
+        run_on_hardware=False,
+        time_step_s=1e-3,
+        inverse_dynamics_pid_gains=id_pid_gains,
+    )
+
+    with pytest.raises(AssertionError):
+        create_lite6_pliant(config=config)
 
 
 if __name__ == "__main__":

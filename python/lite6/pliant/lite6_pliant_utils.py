0b5eb86ca2d9c52059280946deb0eefcdd9cf99a
shrenikm <shrenik95@gmail.com>
2024-02-24 18:34:19 -0800

    demux done

--- python/lite6/pliant/lite6_pliant_utils.py
+++ python/lite6/pliant/lite6_pliant_utils.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 import attr
 import numpy as np
-from pydrake.common.value import Value
+from pydrake.common.value import AbstractValue, Value
 from pydrake.multibody.plant import MultibodyPlantConfig
 from pydrake.systems.framework import BasicVector, Context, LeafSystem
 
@@ -12,10 +12,15 @@ from python.common.control.constructs import PIDGains
 from python.lite6.utils.lite6_model_utils import (
     LITE6_DOF,
     Lite6ControlType,
+    Lite6GripperStatus,
     Lite6ModelType,
     create_lite6_state,
+    get_gripper_status_from_lite6_state,
     get_joint_positions_from_lite6_state,
+    get_joint_velocities_from_lite6_state,
+    get_lite6_num_positions,
     get_lite6_num_states,
+    get_lite6_num_velocities,
 )
 
 LITE6_PLIANT_SUPPORTED_MODEL_TYPES = (
@@ -30,6 +35,9 @@ LITE6_PLIANT_GRIPPER_STATUS_DESIRED_IP_NAME = "gripper_status_desired_input"
 
 LITE6_PLIANT_STATE_ESTIMATED_IP_NAME = "state_estimated_input"
 LITE6_PLIANT_STATE_DESIRED_OP_NAME = "state_desired_output"
+LITE6_PLIANT_POSITIONS_ESTIMATED_OP_NAME = "positions_estimated_output"
+LITE6_PLIANT_VELOCITIES_ESTIMATED_OP_NAME = "velocities_estimated_output"
+LITE6_PLIANT_GRIPPER_STATUS_ESTIMATED_OP_NAME = "gripper_status_estimated_output"
 
 LITE6_PLIANT_MULTIPLEXER_PREFIX = "mult_"
 LITE6_PLIANT_MULTIPLEXER_PD_IP_NAME = (
@@ -100,7 +108,7 @@ class Lite6PliantMultiplexer(LeafSystem):
         context: Context,
         output_vector: BasicVector,
     ) -> None:
-        state_multiplexed_vector = np.zeros(self.num_total_states, dtype=np.float64)
+        state_output_vector = np.zeros(self.num_total_states, dtype=np.float64)
 
         state_estimated_vector = self._se_input_port.Eval(context)
         positions_desired_vector = self._pd_input_port.Eval(context)
@@ -117,7 +125,7 @@ class Lite6PliantMultiplexer(LeafSystem):
             # The gripper values are set according to the desired flag. Desired gripper
             # velocity will be 0 with the positions being open/closed according to the
             # GSD port input.
-            state_multiplexed_vector = create_lite6_state(
+            state_output_vector = create_lite6_state(
                 lite6_model_type=self.config.lite6_model_type,
                 positions_vector=positions_desired_vector,
                 velocities_vector=velocities_desired_vector,
@@ -132,7 +140,7 @@ class Lite6PliantMultiplexer(LeafSystem):
                 lite6_model_type=self.config.lite6_model_type,
                 state_vector=state_estimated_vector,
             )
-            state_multiplexed_vector = create_lite6_state(
+            state_output_vector = create_lite6_state(
                 lite6_model_type=self.config.lite6_model_type,
                 positions_vector=positions_estimated_vector,
                 velocities_vector=velocities_desired_vector,
@@ -142,5 +150,92 @@ class Lite6PliantMultiplexer(LeafSystem):
             raise NotImplementedError
 
         output_vector.SetFromVector(
-            value=state_multiplexed_vector,
+            value=state_output_vector,
         )
+
+
+class Lite6PliantDeMultiplexer(LeafSystem):
+    def __init__(
+        self,
+        config: Lite6PliantConfig,
+    ):
+        super().__init__()
+
+        self.config = config
+        self.num_total_states = get_lite6_num_states(
+            lite6_model_type=config.lite6_model_type,
+        )
+
+        self._se_input_port = self.DeclareVectorInputPort(
+            name=LITE6_PLIANT_MULTIPLEXER_SE_IP_NAME,
+            size=self.num_total_states,
+        )
+
+        self.DeclareVectorOutputPort(
+            name=LITE6_PLIANT_POSITIONS_ESTIMATED_OP_NAME,
+            size=LITE6_DOF,
+            calc=self.compute_positions_estimated_output,
+        )
+        self.DeclareVectorOutputPort(
+            name=LITE6_PLIANT_VELOCITIES_ESTIMATED_OP_NAME,
+            size=LITE6_DOF,
+            calc=self.compute_velocities_estimated_output,
+        )
+        self.DeclareAbstractOutputPort(
+            name=LITE6_PLIANT_GRIPPER_STATUS_ESTIMATED_OP_NAME,
+            alloc=lambda: Lite6GripperStatus.CLOSED,
+            calc=self.compute_gripper_status_estimated_output,
+        )
+
+    def compute_positions_estimated_output(
+        self,
+        context: Context,
+        output_vector: BasicVector,
+    ) -> None:
+        state_estimated_vector = self._se_input_port.Eval(context)
+
+        # TODO: Remove
+        assert isinstance(state_estimated_vector, np.ndarray)
+
+        positions_output_vector = get_joint_positions_from_lite6_state(
+            lite6_model_type=self.config.lite6_model_type,
+            state_vector=state_estimated_vector,
+        )
+        output_vector.SetFromVector(
+            value=positions_output_vector,
+        )
+
+    def compute_velocities_estimated_output(
+        self,
+        context: Context,
+        output_vector: BasicVector,
+    ) -> None:
+        state_estimated_vector = self._se_input_port.Eval(context)
+
+        # TODO: Remove
+        assert isinstance(state_estimated_vector, np.ndarray)
+
+        velocities_output_vector = get_joint_velocities_from_lite6_state(
+            lite6_model_type=self.config.lite6_model_type,
+            state_vector=state_estimated_vector,
+        )
+        output_vector.SetFromVector(
+            value=velocities_output_vector,
+        )
+
+    def compute_gripper_status_estimated_output(
+        self,
+        context: Context,
+        output_value: AbstractValue,
+    ) -> None:
+        state_estimated_vector = self._se_input_port.Eval(context)
+
+        # TODO: Remove
+        assert isinstance(state_estimated_vector, np.ndarray)
+
+        lite6_gripper_status = get_gripper_status_from_lite6_state(
+            lite6_model_type=self.config.lite6_model_type,
+            state_vector=state_estimated_vector,
+        )
+
+        output_value.set_value(lite6_gripper_status)

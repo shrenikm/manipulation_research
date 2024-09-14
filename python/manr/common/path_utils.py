7364cdca98b2e7caf030d1ac690d9b0493a8b5c8
shrenikm <shrenik95@gmail.com>
2024-09-13 23:42:32 -0700

    more

--- python/manr/common/path_utils.py
+++ python/manr/common/path_utils.py
@@ -1,6 +1,7 @@
 """
 Utilities for working with paths.
 """
+
 import os
 from typing import List
 
@@ -10,6 +11,9 @@ from manr.common.custom_types import DirPath, FilePath
 def list_directories_in_path(
     path: DirPath,
 ) -> List[DirPath]:
+    """
+    Returns a list of all directories in the given path.
+    """
     return [
         file_or_directory
         for file_or_directory in os.listdir(path)
@@ -20,8 +24,20 @@ def list_directories_in_path(
 def list_files_in_path(
     path: DirPath,
 ) -> List[FilePath]:
+    """
+    Returns a list of all files in the given path.
+    """
     return [
         file_or_directory
         for file_or_directory in os.listdir(path)
         if os.path.isfile(os.path.join(path, file_or_directory))
     ]
+
+
+def create_directory_if_not_exists(
+    directory_path: DirPath,
+) -> None:
+    """
+    Creates a directory at the given path if it does not already exist.
+    """
+    os.makedirs(directory_path, exist_ok=True)
---
+++ python/manr/common/tests/test_path_utils.py
@@ -0,0 +1,4 @@
+from manr.common.testing_utils import execute_pytest_file
+
+if __name__ == "__main__":
+    execute_pytest_file()
